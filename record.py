b4d45d480ca74b29db2e0e7059822dfcace47dd0
wMonarch <104732754+wMonarch@users.noreply.github.com>
2022-05-10 22:46:17 +0700

    Update record.py

--- record.py
+++ record.py
@@ -1,45 +1,22 @@
-import speech_recognition as sr
-from tkinter import *
 import pyaudio
 import keyboard
 import numpy as np
 from scipy.io import wavfile
-import threading
+import speech_recognition as sr
 
 
-class Recorder(): # Класс для записи аудио
+class Recorder():
     def __init__(self):
         self.audio_format = pyaudio.paInt16
         self.channels = 1
         self.sample_rate = 44100
         self.chunk = int(0.03*self.sample_rate)
         self.filename = 'Space.wav'
-        self.START_KEY = 'Space'
-        self.STOP_KEY = 'alt'
-
-    def Gui(self): # интерфейс
-        root = Tk()
-        root.title('AudioTranslator')
-        root.geometry('450x300')
-        root.resizable(width=False, height=False)
-        canvas = Canvas(root, height=450, width=300)
-        canvas.pack()
-        frame = Frame(root, bg='white')
-        frame.place(relheight=1, relwidth=1)
-        title = Label(frame, text='AudioTranslator', bg='gray', font='40')
-        title.pack()
-        bt1 = Label(frame, text='Нажмите клавишу "Space" для записи аудио', bg='white', font='45')
-        bt2 = Label(frame, text='Нажмите клавишу "Alt" для прекращения записи', bg='white', font='45')
-        bt3 = Button(frame, text='...', width=30, height=3)
-        bt4 = Button(frame, text='...', width=30, height=3)
-        bt1.pack()
-        bt2.pack()
-        bt3.pack()
-        bt4.pack()
-        root.mainloop()
+        self.START_KEY = 's'
+        self.STOP_KEY = 'q'
 
 
-    def record(self): # функция
+    def record(self):
         recorded_data = []
         p = pyaudio.PyAudio()
 
@@ -51,7 +28,7 @@ class Recorder(): # Класс для записи аудио
             recorded_data.append(data)
             if keyboard.is_pressed(self.STOP_KEY):
                 print("Stop recording")
-                # начать и закончить стрим
+                # stop and close the stream
                 stream.stop_stream()
                 stream.close()
                 p.terminate()
@@ -63,25 +40,23 @@ class Recorder(): # Класс для записи аудио
                 break
 
 
-    def listen(self): # функция записи
+    def listen(self):
         print(f"Press `{self.START_KEY}` to start and `{self.STOP_KEY}` to quit!")
         while True:
             if keyboard.is_pressed(self.START_KEY):
                 self.record()
                 break
 
-
-def translate_record(): # Функция перевода голоса в текст
-    r = sr.Recognizer()
-    translate = sr.AudioFile('Space.wav')
-    with translate as source:
-        audio = r.record(source)
-    query = r.recognize_google(audio, language='ru-RU')
-    file = open('translate.txt', 'w')
-    file.write(query)
-    file.close()
-
+    def translate_record(self): # Функция перевода голоса в текст
+        r = sr.Recognizer()
+        translate = sr.AudioFile('Space.wav')
+        with translate as source:
+            audio = r.record(source)
+        query = r.recognize_google(audio, language='ru-RU')
+        file = open('translate.txt', 'w')
+        file.write(query)
+        file.close()
 
 recorder = Recorder()
-
-
+recorder.listen()
+recorder.translate_record()
